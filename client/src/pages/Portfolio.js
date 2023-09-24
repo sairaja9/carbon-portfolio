1e20a3b1ae83b8f7661dfcd2c75a6148190f866a
Sai K Raja <saikraja9@gmail.com>
2023-09-24 07:12:26 -0500

    Fixing Portfolio

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
+import Graph from '../graphs/1111.png';
+
 export default function Portfolio() {
-    let filename = "../graphs/1111.png";
+    let filename = "./graphs/1111.png";
     let quantity, budget, risk, viz = "1";
     let handleChange = (event) => {
         quantity = event.target.quantityField.value;
         budget = event.target.budgetField.value;
         risk = event.target.riskField.value;
         viz = "1";
-        filename = "../graphs/" + quantity + budget + risk + viz + ".png";
+        filename = "./graphs/" + quantity + budget + risk + viz + ".png";
         console.log(filename);
     }
 
     let handleVizChange = (event) => {
         viz = event.target.value;
-        filename = "../graphs/" + quantity + budget + risk + viz + ".png";
+        filename = "./graphs/" + quantity + budget + risk + viz + ".png";
     }
     return (
         <section className='portfolio'>
@@ -48,7 +50,7 @@ export default function Portfolio() {
                         <button className="submit-button" type="submit">Generate Graphs</button>
                     </form>
                     <div className='graph-container'>
-                        <img className="graph-img" src={require(filename)} alt="graph"/>
+                        <img className="graph-img" src={Graph} alt="graph"/>
                     </div>
                 </div>
                 <form className='viz-selector' onChange={handleVizChange}>
